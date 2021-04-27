283608785ae8c2cf06f47cf8fb864a945b361c86
shubhamnatekar <shubhamnatekar9221@gmail.com>
2021-04-27 23:27:06 +0530

    code updated

--- app/app.py
+++ app/app.py
@@ -1,12 +1,9 @@
 from flask import Flask, jsonify
 from datetime import datetime
 import socket
-import urllib.request
 import json
 
 app = Flask(__name__)
-
-pune_ip='14.140.42.210' # pune city ip address
 
 @app.route('/pucsd')
 def myapi():
@@ -15,18 +12,13 @@ def myapi():
     ip=socket.gethostbyname(hostname) 
     now = datetime.now()
         
-    # Sending an API request
-    # response = urllib.request.urlopen("http://ipwhois.app/json/"+ip)
-    response = urllib.request.urlopen("http://ipwhois.app/json/"+pune_ip)
-    ipgeolocation = json.load(response)
-
     data=[{
     	"Current Time in IST" : now.strftime("%H:%M:%S"),
 	"IP Address" : ip,
 	"Hostname" : hostname,
-	"City" : ipgeolocation["city"],
-	"Region" : ipgeolocation["region"],
-	"Country" : ipgeolocation["country"] 
+	"City" : "Pune",
+	"Region" : "Maharashtra",
+	"Country" : "India" 
     }]
     return jsonify(data)
 
